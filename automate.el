13d5fbcd937e39cf2a2177e66c2eb6fab18ccc32
Joel Svensson <svenssonjoel@yahoo.se>
2018-08-04 20:21:17 +0200

    planning

---
+++ automate.el
@@ -0,0 +1,22 @@
+;; automate.el --- Automate tasks
+
+;; Copyright (C) 2018 
+;; Author: Joel Svensson <svenssonjoel@yahoo.se> 
+
+;; This file is part of Emacs-Automate.
+
+;; Emacs-Automate is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Emacs-Automate is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Emacs-Automate.  If not, see <https://www.gnu.org/licenses/>.
+
+(require 'cl)
+
