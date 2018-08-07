c86cde9e430d3d7673430bc721fd54fffa2f30ca
Joel Svensson <svenssonjoel@yahoo.se>
2018-08-07 10:55:56 +0200

    planning

--- automate.el
+++ automate.el
@@ -21,15 +21,45 @@
 (require 'cl)
 
 
+;; ------------------------------------------------------------
+;; Structs
+
+(cl-defstruct automate-machine-info
+  name        ;; Name of the machine
+  description ;;
+  ip          ;;
+  arch)       ;; Architecture descriptions
+
+(cl-defstruct automate-job-info
+  name           ;; Name of the job
+  machine-filter ;; Filter for deciding what machine to use (Arch . x86) (Arch . ARM) (Name . "name") etc
+  periodic       ;; 'Hourly, 'Daily, 'Weekly, etc
+  command        ;; Task to perform
+  status)        ;; 'Waiting, 'Running, 'Finished-success, 'Finished-failure
+  
+  
+
 
 ;; ------------------------------------------------------------
 ;; State
 
-(defvar automate-machines-list ())
+(defvar automate-machines-list      '())
+(defvar automate-machines-idle-list '())
+(defvar automate-machines-busy-list '())
 
+
+(defvar automate-jobs-list '())
 
 ;; ------------------------------------------------------------
 ;; CODE
+
+(defun automate-machine-info-string (machine-info)
+  "Convert an automate-machine-info object to a string"
+  (format "Name: %s\nDesc: %s\nIP: %s\nArch: %s\n"
+	  (automate-machine-info-name machine-info)
+	  (automate-machine-info-description machine-info)
+	  (automate-machine-info-ip machine-info)
+	  (automate-machine-info-arch machine-info)))
 
 
 ;; ------------------------------------------------------------
