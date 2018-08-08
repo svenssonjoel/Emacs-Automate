7897ab7f33802020e76279a6abcd2aa53f1a7203
Joel Svensson <svenssonjoel@yahoo.se>
2018-08-08 19:52:36 +0200

    planning

--- automate.el
+++ automate.el
@@ -28,8 +28,9 @@
   name        ;; Name of the machine
   description ;;
   ip          ;;
-  arch)       ;; Architecture descriptions
-
+  arch        ;; Architecture descriptions
+  status)     ;; 'Available, 'Offline, 'Online etc
+  
 (cl-defstruct automate-job-info
   name           ;; Name of the job
   machine-filter ;; Filter for deciding what machine to use (Arch . x86) (Arch . ARM) (Name . "name") etc
@@ -50,17 +51,48 @@
 (defvar automate-login "automate") ;; username used by automate when loging into remote machine
                                    ;; Set this in .emacs file?
 
+;; Testing
+(setq automate-machines-list
+      (cons 
+       (make-automate-machine-info :name "Test-Machine"
+				   :description "Testing"
+				   :ip "192.168.0.46"
+				   :arch 'x86
+				   :status 'Offline)
+       '()))
+ 
+      
+	
+
 
 ;; ------------------------------------------------------------
 ;; CODE
 
 (defun automate-machine-info-string (machine-info)
   "Convert an automate-machine-info object to a string"
-  (format "Name: %s\nDesc: %s\nIP: %s\nArch: %s\n"
+  (format "Name: %s\nDesc: %s\nIP: %s\nArch: %s\nStatus: %s\n--\n"
 	  (automate-machine-info-name machine-info)
 	  (automate-machine-info-description machine-info)
 	  (automate-machine-info-ip machine-info)
-	  (automate-machine-info-arch machine-info)))
+	  (automate-machine-info-arch machine-info)
+	  (automate-machine-info-status machine-info)))
+
+(defun automate-print-machines-list (buffer)
+  "list all machines and their status"
+  (dolist (elt automate-machines-list nil)
+    (with-current-buffer buffer
+      (insert (automate-machine-info-string elt)))))
+  
+
+;; TODO: add sentinel and checking of ping result and update machines-info
+(defun automate-ping-machines ()
+  "ping all machines in the machines list"
+  (dolist (elt automate-machines-list nil)
+      (make-process :name "ping-proc"
+		    :command (list "ping" "-c 1" (automate-machine-info-ip elt))
+		    :buffer "apa")))
+
+      
 
 ;; ------------------------------------------------------------
 ;; Interface
